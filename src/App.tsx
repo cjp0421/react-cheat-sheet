8264715654608802e580d8de5175002eceeea9c5
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-09-30 20:44:00 -0500

    added router to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
+import Layout from './components/Layout'
+import AppRoutes from '../routes'
 
 function App() {
 
-
   return (
-    <></>
+    <Router>
+      <Layout>
+        <h1>Hiya</h1>
+        <AppRoutes />
+      </Layout>
+    </Router>
   )
 }
 
