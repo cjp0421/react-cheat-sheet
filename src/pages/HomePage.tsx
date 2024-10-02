1da427b5448896b58dbc44750df2c8b517fc7789
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-10-01 20:05:25 -0500

    implement useNavigate and build out basic home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,29 @@
-import { Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 // import CustomModal from '../components/CustomModal'
 // import CustomDropdown from '../components/CustomDropdown'
 
 const HomePage: React.FC = () => {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <Typography variant='h4' gutterBottom>
-                Home Page
+        <Box sx={{ p: 3, mt: 2 }}>
+            <Typography variant='h1' gutterBottom sx={{ p: 2, fontSize: 48 }}>
+                Welcome to my React + TypeScript Cheat Sheet!
             </Typography>
-        </div>
+            <Typography variant='h4' gutterBottom sx={{ p: 2 }}>
+                Learn About Event Handling!
+            </Typography>
+            <Button onClick={() => navigate('/event-handling')}>Go to Event Handling</Button>
+            <Typography variant='h4' gutterBottom sx={{ p: 2 }}>
+                Learn About Hooks!
+            </Typography>
+            <Button onClick={() => navigate('/hooks')}>Go to Hooks</Button>
+            <Typography variant='h4' gutterBottom sx={{ p: 2 }}>
+                Learn About State Management!
+            </Typography>
+            <Button onClick={() => navigate('/state-management')}>Go to State Management</Button>
+        </Box>
     )
 }
 
