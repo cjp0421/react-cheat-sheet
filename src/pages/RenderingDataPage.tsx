0a92185529c6dc6a4798e0e193e4ab6746ee80c7
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-10-03 21:26:52 -0500

    implemented two Fabs and two modals using the modal component for filter and map and added a filter example

--- src/pages/RenderingDataPage.tsx
+++ src/pages/RenderingDataPage.tsx
@@ -1,5 +1,6 @@
-import { Box, Typography } from '@mui/material';
-// import ModalTemplate from '../components/ModalTemplate'
+import { Box, Typography, Fab } from '@mui/material';
+import { useState } from 'react';
+import ModalTemplate from '../components/Modal';
 // import CustomDropdown from '../components/CustomDropdown'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
@@ -75,31 +76,96 @@ return (
 export default BotchedMission;
 `
 
-const RenderingDataPage: React.FC = () => {
+const basicFilterAndMapEx: string = `
+const DrEmployees = () => {
+    const planetExpressEmployees = [
+    'Dr. Hubert Farnsworth', 
+    'Turanga Leela', 
+    'Phillip J. Fry', 
+    'Dr. Amy Wong', 
+    'Hermes Conrad', 
+    'Dr. John Zoidberg', 
+    'Lord Nibbler'
+    ];
+    
+    const employeesWithDoctorate = planetExpressEmployees.filter(
+        employee => employee.startsWith('Dr.')
+    );
+
     return (
-        <Box sx={{ p: 1, pt: 11, maxWidth: '70vw', display: 'flex', flexDirection: 'column' }}>
-            <Typography variant='h4' gutterBottom sx={{ width: '70vw' }}>
-                Rendering Data
-            </Typography>
-            <Typography variant='subtitle1' sx={{ pt: 1, width: '70vw' }} >
-                .map()
-            </Typography>
-            <SyntaxHighlighter language="jsx" style={materialDark}>
-                {mapSimpleEx}
-            </SyntaxHighlighter>
-            <Typography variant='subtitle1' sx={{ pt: 1, width: '70vw' }}>
-                .map() in a component with TypeScript
-            </Typography>
-            <SyntaxHighlighter language="jsx" style={materialDark}>
-                {mapSimpleAndTypeScriptEx}
-            </SyntaxHighlighter>
-            <Typography variant='subtitle1' sx={{ pt: 1, width: '70vw' }}>
+        <div>
+            <h2>Employees with a Doctorate</h2>
+            <ul>
+                {employeesWithDoctorate.map((employee, index) => (
+                    <li key={index}>{employee}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}    
+
+export default DrEmployees;
+`
+
+const RenderingDataPage: React.FC = () => {
+    const [isModalOpen, setIsModalOpen] = useState<string | null>(null);
+
+    const handleOpen = (modal: string) => {
+        setIsModalOpen(modal);
+    }
+
+    const handleClose = () => {
+        setIsModalOpen(null)
+    }
+
+    return (
+        <Box sx={{ p: 1, pt: 11, maxWidth: '70vw', display: 'flex', flexDirection: 'column', alignItems: "center" }}>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%', alignItems: 'center' }}>
+                <Typography variant='h4' gutterBottom sx={{ width: '45vw' }}>
+                    Rendering Data
+                </Typography>
+                <Box>
+                    <Fab variant='extended' size="medium" color='secondary' sx={{ width: '25vw', p: 1, marginBottom: '10px' }} onClick={() => handleOpen('map')}>Learn More About .map()</Fab>
+                    <Fab variant='extended' size="medium" color='success' sx={{ width: '25vw', p: 1 }} onClick={() => handleOpen('filter')}>Learn More About .filter()</Fab>
+                </Box>
+            </Box>
+
+            <ModalTemplate onClose={handleClose} open={isModalOpen === 'map'} title=".map()">
+                <Typography variant="body1">
+                    This modal contains information about the `.map()` function and how it is used in React to transform arrays into lists of elements.
+                </Typography></ModalTemplate>
+            <ModalTemplate onClose={handleClose} open={isModalOpen === 'filter'} title=".filter()">
+                <Typography variant="body1">
+                    This modal provides additional information regarding other JavaScript array methods that are useful in React, such as `.filter()` and `.reduce()`.
+                </Typography></ModalTemplate>
+            <Box>
+                <Typography variant='subtitle1' sx={{ pt: 1 }} >
+                    .map()
+                </Typography>
+                <SyntaxHighlighter language="jsx" style={materialDark}>
+                    {mapSimpleEx}
+                </SyntaxHighlighter>
+            </Box>
+            <Box>
+                <Typography variant='subtitle1' sx={{ pt: 1 }}>
+                    .map() in a component with TypeScript
+                </Typography>
+                <SyntaxHighlighter language="jsx" style={materialDark}>
+                    {mapSimpleAndTypeScriptEx}
+                </SyntaxHighlighter>
+            </Box>
+            <Typography variant='subtitle1' sx={{ pt: 1 }}>
                 .map() over array of objects while updating properties on the object in a component with TypeScript
             </Typography>
             <SyntaxHighlighter language="jsx" style={materialDark}>
                 {mapObjectAndTypeScriptEx}
             </SyntaxHighlighter>
-
+            <Typography variant='subtitle1' sx={{ pt: 1 }}>
+                .filter() and .map()
+            </Typography>
+            <SyntaxHighlighter language='jsx' style={materialDark}>
+                {basicFilterAndMapEx}
+            </SyntaxHighlighter>
         </Box >
     )
 }
