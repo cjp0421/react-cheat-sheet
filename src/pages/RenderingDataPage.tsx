5812c1cbf3821a05a7d8f4501eab1c4235d82bf6
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-10-03 21:44:29 -0500

    put code examples in json file

--- src/pages/RenderingDataPage.tsx
+++ src/pages/RenderingDataPage.tsx
@@ -4,108 +4,7 @@ import ModalTemplate from '../components/Modal';
 // import CustomDropdown from '../components/CustomDropdown'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
-
-const mapSimpleEx: string = `
-const crew = ['Leela', 'Fry', 'Bender'];
-return (
-    <ul>
-        {crew.map((crewMember, index) => (
-            <li key={index}>{crewMember}</li>
-        ))}
-    </ul>
-);
-`
-
-const mapSimpleAndTypeScriptEx: string = `
-const crewList: React.FC = () => {
-    const crew: string[] = ['Leela', 'Fry', 'Bender']; 
-
-    return (
-        <ul>
-            {crew.map((crewMember: string, index: number) => (
-                <li key={index}>{crewMember}</li>
-            ))}
-        </ul>
-    );
-};
-
-export default CrewList;
-`
-
-const mapObjectAndTypeScriptEx: string = `
-interface Crew {
-    id: number;
-    name: string;
-    isInjured: boolean;
-    nearDeathExperiences: number;    
-}
-
-const crew: Crew[] = [
-    { id: 1, name: 'Leela', isInjured: false, nearDeathExperiences: 0 },
-    { id: 2, name: 'Fry', isInjured: false, nearDeathExperiences: 0 },
-    { id: 3, name: 'Bender', isInjured: false, nearDeathExperiences: 0 },
-];
-
-const BotchedMission: React.FC = () => {
-const updatedCrew: Crew[] = crew.map((crewMemeber: Crew) => {
-    return {
-    ...crew,
-    isInjured: true,
-    nearDeathExperiences: nearDeathExperiences + 1,
-    };
-});
-
-return (
-    <div>
-        <h2>Post-Mission Crew Status</h2>
-        <ul>
-            {updatedCrew.map((crewMember: Crew) => (
-            <ul>
-                <li key={crew.id}>
-                        Crew Member: {crewMember.name} <br/>
-                        Injured: {crewMember.isInjured ? 'Yes' : 'No'} <br/>
-                        # of Near Death Experiences: {crewMember.nearDeathExperiences} <br/>
-                </li>
-            <ul>
-            ))}
-        </ul>
-    </div>
- );
-};
-
-export default BotchedMission;
-`
-
-const basicFilterAndMapEx: string = `
-const DrEmployees = () => {
-    const planetExpressEmployees = [
-    'Dr. Hubert Farnsworth', 
-    'Turanga Leela', 
-    'Phillip J. Fry', 
-    'Dr. Amy Wong', 
-    'Hermes Conrad', 
-    'Dr. John Zoidberg', 
-    'Lord Nibbler'
-    ];
-    
-    const employeesWithDoctorate = planetExpressEmployees.filter(
-        employee => employee.startsWith('Dr.')
-    );
-
-    return (
-        <div>
-            <h2>Employees with a Doctorate</h2>
-            <ul>
-                {employeesWithDoctorate.map((employee, index) => (
-                    <li key={index}>{employee}</li>
-                ))}
-            </ul>
-        </div>
-    );
-}    
-
-export default DrEmployees;
-`
+import { map, filter } from './../data/codeExamples.json'
 
 const RenderingDataPage: React.FC = () => {
     const [isModalOpen, setIsModalOpen] = useState<string | null>(null);
@@ -143,7 +42,7 @@ const RenderingDataPage: React.FC = () => {
                     .map()
                 </Typography>
                 <SyntaxHighlighter language="jsx" style={materialDark}>
-                    {mapSimpleEx}
+                    {map.simpleExample}
                 </SyntaxHighlighter>
             </Box>
             <Box>
@@ -151,20 +50,20 @@ const RenderingDataPage: React.FC = () => {
                     .map() in a component with TypeScript
                 </Typography>
                 <SyntaxHighlighter language="jsx" style={materialDark}>
-                    {mapSimpleAndTypeScriptEx}
+                    {map.simpleAndTypeScriptExample}
                 </SyntaxHighlighter>
             </Box>
             <Typography variant='subtitle1' sx={{ pt: 1 }}>
                 .map() over array of objects while updating properties on the object in a component with TypeScript
             </Typography>
             <SyntaxHighlighter language="jsx" style={materialDark}>
-                {mapObjectAndTypeScriptEx}
+                {map.objectAndTypeScriptExample}
             </SyntaxHighlighter>
             <Typography variant='subtitle1' sx={{ pt: 1 }}>
                 .filter() and .map()
             </Typography>
             <SyntaxHighlighter language='jsx' style={materialDark}>
-                {basicFilterAndMapEx}
+                {filter.basicFilterAndMapExample}
             </SyntaxHighlighter>
         </Box >
     )
