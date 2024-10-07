b3731b24fe3a36117d2bb70dd05ffb9930afc633
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-10-06 20:22:40 -0500

    Refactored drawer to open/close using state and a handler, updated the appbar to be full length dependant on the drawer's state, and added css transitions for drawer and appbar, added icon for open/closing drawer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,24 +1,46 @@
-import { AppBar, Box, Drawer, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Drawer, IconButton, Toolbar, Typography } from '@mui/material';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const drawerWidth = 200;
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+    const handleDrawerToggle = () => {
+        setIsDrawerOpen(!isDrawerOpen)
+    }
+
     return (
         <Box sx={{ display: 'flex' }}>
-            <AppBar position='fixed' sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}>
+            <AppBar position='fixed' sx={{ transition: 'width 0.3s', width: isDrawerOpen ? `calc(100% - ${drawerWidth}px)` : '100%', ml: `${drawerWidth}px` }}>
                 <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
                     <Typography variant='h4'>
                         React + TypeScript
                     </Typography>
                 </Toolbar>
             </AppBar>
 
-            <Drawer variant='permanent' sx={{
-                width: drawerWidth,
-                flexShrink: 0,
-                [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' }
-            }}>
+            <Drawer
+                variant='persistent'
+                open={isDrawerOpen}
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+                    transition: 'width 0.3s',
+                }}>
                 <Toolbar />
                 <Box sx={{ width: drawerWidth, overflow: 'auto', pl: 1 }}>
                     <Typography><Link to="/">Home</Link></Typography>
