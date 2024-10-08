42f9258e4d5a7fa2ddda6d97d8c0ca6d0bab00f9
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-10-07 21:49:20 -0500

    added and displayed two onClick examples

--- src/pages/EventHandlingPage.tsx
+++ src/pages/EventHandlingPage.tsx
@@ -1,14 +1,53 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 // import ModalTemplate from '../components/ModalTemplate'
 // import CustomDropdown from '../components/CustomDropdown'
 
 const EventHandlingPage: React.FC = () => {
+
+    const simpleOnClickExample = `
+    <button onClick={() => alert('Auto Destruct Activated!')}>
+        Don't Click Me!
+</button>
+    `;
+
+    const onClickExampleWithHandlerTypeScript = `
+    const AutoDestruct: React.FC = () => {
+        const handleDoomClick = (): void => {
+            alert('Auto Destruct Activated!');
+        }
+    
+        return (
+            <button onClick={handleDoomClick}>
+                Don't Click Me!
+            </button>
+        );
+    };
+    
+    export default AutoDestruct;
+    `;
+
     return (
-        <div>
-            <Typography variant='h4' gutterBottom>
-                Event Handling
-            </Typography>
-        </div>
+        <Box>
+            <Box>
+                <Typography variant='h4' gutterBottom>
+                    Event Handling
+                </Typography>
+            </Box>
+            <Box>
+                <Typography variant='subtitle1'>
+                    onClick event
+                </Typography>
+                <SyntaxHighlighter language='jsx' style={materialDark}>
+                    {simpleOnClickExample.trim()}
+                </SyntaxHighlighter>
+                <SyntaxHighlighter language='jsx' style={materialDark}>
+                    {onClickExampleWithHandlerTypeScript.trim()}
+                </SyntaxHighlighter>
+            </Box>
+        </Box>
+
     )
 }
 
