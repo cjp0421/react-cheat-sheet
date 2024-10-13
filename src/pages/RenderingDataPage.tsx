f99d7f4e659dec3430a581d15be6bbd5216d6c2f
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-10-13 17:16:29 -0500

    added functionality to use a dropdown to determine which of the examples the user wants to view, updated fab styles, and updated the definition of the filter explanation modal and the reduce explanation modal

--- src/pages/RenderingDataPage.tsx
+++ src/pages/RenderingDataPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Fab, useMediaQuery } from '@mui/material';
+import { Box, Typography, Fab, useMediaQuery, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { useState } from 'react';
 import ModalTemplate from '../components/Modal';
@@ -10,6 +10,8 @@ import { reduceExample } from '../data/eventHandlingExamples';
 
 const RenderingDataPage: React.FC = () => {
     const [isModalOpen, setIsModalOpen] = useState<string | null>(null);
+    const [selectedCategory, setSelectedCategory] = useState<string>('all');
+
     const theme = useTheme();
     const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
 
@@ -18,9 +20,120 @@ const RenderingDataPage: React.FC = () => {
     }
 
     const handleClose = () => {
-        setIsModalOpen(null)
+        setIsModalOpen(null);
     }
 
+    const handleCategoryChange = (event: SelectChangeEvent<string>) => {
+        setSelectedCategory(event.target.value)
+    }
+
+    const renderSelectedExamples = (): JSX.Element | null => {
+        if (selectedCategory === 'all') {
+            return (
+                <>
+                    <Box>
+                        <Typography variant='subtitle1' sx={{ pt: 1 }} >
+                            .map()
+                        </Typography>
+                        <SyntaxHighlighter language="jsx" style={materialDark}>
+                            {map.simpleExample}
+                        </SyntaxHighlighter>
+                    </Box>
+                    <Box>
+                        <Typography variant='subtitle1' sx={{ pt: 1 }}>
+                            .map() in a component with TypeScript
+                        </Typography>
+                        <SyntaxHighlighter language="jsx" style={materialDark}>
+                            {map.simpleAndTypeScriptExample}
+                        </SyntaxHighlighter>
+                    </Box>
+                    <Typography variant='subtitle1' sx={{ pt: 1 }}>
+                        .map() over array of objects while updating properties on the object in a component with TypeScript
+                    </Typography>
+                    <SyntaxHighlighter language="jsx" style={materialDark}>
+                        {map.objectAndTypeScriptExample}
+                    </SyntaxHighlighter>
+                    <Typography variant='subtitle1' sx={{ pt: 1 }}>
+                        .filter() and .map()
+                    </Typography>
+                    <SyntaxHighlighter language='jsx' style={materialDark}>
+                        {filter.basicFilterAndMapExample}
+                    </SyntaxHighlighter>
+                    <Typography variant='subtitle1' sx={{ pt: 1 }}>
+                        .filter() and .map() with TypeScript
+                    </Typography>
+                    <SyntaxHighlighter language='jsx' style={materialDark}>
+                        {filter.typescriptFilterAndMapExample}
+                    </SyntaxHighlighter>
+                    <Typography variant='subtitle1' sx={{ pt: 1 }}>
+                        .reduce()
+                    </Typography>
+                    <SyntaxHighlighter language='jsx' style={materialDark}>
+                        {reduceExample}
+                    </SyntaxHighlighter>
+                </>
+            )
+        }
+        if (selectedCategory === 'map') {
+            return (
+                <>
+                    <Typography variant='subtitle1' sx={{ pt: 1 }}>
+                        .map() - Simple Example
+                    </Typography>
+                    <SyntaxHighlighter language="jsx" style={materialDark}>
+                        {map.simpleExample}
+                    </SyntaxHighlighter>
+
+                    <Typography variant='subtitle1' sx={{ pt: 1 }}>
+                        .map() - TypeScript Example
+                    </Typography>
+                    <SyntaxHighlighter language="jsx" style={materialDark}>
+                        {map.simpleAndTypeScriptExample}
+                    </SyntaxHighlighter>
+
+                    <Typography variant='subtitle1' sx={{ pt: 1 }}>
+                        .map() - Array of Objects with TypeScript
+                    </Typography>
+                    <SyntaxHighlighter language="jsx" style={materialDark}>
+                        {map.objectAndTypeScriptExample}
+                    </SyntaxHighlighter>
+                </>
+            );
+        }
+        if (selectedCategory === 'filter') {
+            return (
+                <>
+                    <Typography variant='subtitle1' sx={{ pt: 1 }}>
+                        .filter() and .map() - Basic Example
+                    </Typography>
+                    <SyntaxHighlighter language="jsx" style={materialDark}>
+                        {filter.basicFilterAndMapExample}
+                    </SyntaxHighlighter>
+
+                    <Typography variant='subtitle1' sx={{ pt: 1 }}>
+                        .filter() and .map() with TypeScript
+                    </Typography>
+                    <SyntaxHighlighter language="jsx" style={materialDark}>
+                        {filter.typescriptFilterAndMapExample}
+                    </SyntaxHighlighter>
+                </>
+            );
+        }
+        if (selectedCategory === 'reduce') {
+            return (
+                <>
+                    <Typography variant='subtitle1' sx={{ pt: 1 }}>
+                        .reduce() - Example
+                    </Typography>
+                    <SyntaxHighlighter language="jsx" style={materialDark}>
+                        {reduceExample}
+                    </SyntaxHighlighter>
+                </>
+            );
+        }
+        return null;
+    }
+    console.log(isSmallScreen)
     return (
         <Box
             sx={{
@@ -41,6 +154,21 @@ const RenderingDataPage: React.FC = () => {
                 <Typography variant='h4' gutterBottom sx={{ width: '45vw' }}>
                     Rendering Data
                 </Typography>
+                <FormControl sx={{ minWidth: 200, marginRight: '5%' }}>
+                    <InputLabel id="example-category-label">Select Type of Example to View</InputLabel>
+                    <Select
+                        labelId='example-category-label'
+                        value={selectedCategory}
+                        onChange={handleCategoryChange}
+                        label="Select Type of Example to View"
+                    >
+                        <MenuItem value="all">All Examples</MenuItem>
+                        <MenuItem value="map">.map()</MenuItem>
+                        <MenuItem value="filter">.filter()</MenuItem>
+                        <MenuItem value="reduce">.reduce()</MenuItem>
+                    </Select>
+                </FormControl>
+
                 <Box sx={{ display: 'block' }}>
                     <Fab
                         variant='extended'
@@ -49,7 +177,8 @@ const RenderingDataPage: React.FC = () => {
                         sx={{
                             width: `${isSmallScreen ? '15vw' : '25vw'}`,
                             p: 3,
-                            marginBottom: '10px'
+                            marginBottom: '10px',
+                            fontSize: `${isSmallScreen ? 'small' : 'medium'}`,
                         }}
                         onClick={() => handleOpen('map')}
                     >
@@ -63,7 +192,8 @@ const RenderingDataPage: React.FC = () => {
                         sx={{
                             width: `${isSmallScreen ? '15vw' : '25vw'}`,
                             p: 3,
-                            marginBottom: '10px'
+                            marginBottom: '10px',
+                            fontSize: `${isSmallScreen ? 'small' : 'medium'}`
                         }}
                         onClick={() => handleOpen('filter')}
                     >
@@ -74,7 +204,12 @@ const RenderingDataPage: React.FC = () => {
                         variant='extended'
                         size="medium"
                         color='primary'
-                        sx={{ width: `${isSmallScreen ? '15vw' : '25vw'}`, p: 3 }}
+                        sx={{
+                            width: `${isSmallScreen ? '15vw' : '25vw'}`,
+                            p: 3,
+                            marginBottom: '10px',
+                            fontSize: `${isSmallScreen ? 'small' : 'medium'}`
+                        }}
                         onClick={() => handleOpen('reduce')}
                     >
                         {isSmallScreen ? '.reduce()' : 'Learn More About .reduce()'}
@@ -94,57 +229,26 @@ const RenderingDataPage: React.FC = () => {
             <ModalTemplate onClose={handleClose} open={isModalOpen === 'filter'} title=".filter()">
                 <Typography variant="body1">
                     The .filter() method is used to create a new array by conditionally selecting elements from an
-                    exisiting array, based on a provided condition or test. It is commonly used to render a subset of data
-                    in the UI by filtering items that meet specific criteria. The .filter() method is often combined with
-                    the .map() method to dynamically display filtered lists of components, such as search results
-                    or filtered lists in tables or menus.
+                    exisiting array, based on a provided condition or test, and then returning the selected elements as a new array.
+                    The method takes a callback function, which is executed for each element in the array. The callback function should return a boolean.
+                    It is commonly used to render a subset of data in the UI by filtering items that meet specific criteria.
+                    The .filter() method is often combined with the .map() method to dynamically display filtered lists of components,
+                    such as search results or filtered lists in tables or menus.
                 </Typography>
             </ModalTemplate>
             <ModalTemplate onClose={handleClose} open={isModalOpen === 'reduce'} title=".reduce()">
                 <Typography variant="body1">
-                    This will contain information about the .reduce() method.
+                    The .reduce() method is used to iterate over all elements in an array and accumulate, or combine, them into a single value.
+                    This is why it's called "reducing" the array: all the values in an array are reduced to one value. The .reduce() method takes a
+                    callback function and an initial value as arguments. The callback function executes for each element in the array, and it accumulates
+                    a result using the previous value and the current element. The final result is the accumulated value after the method has processed
+                    all elements in the array.
                 </Typography>
             </ModalTemplate>
             <Box>
-                <Typography variant='subtitle1' sx={{ pt: 1 }} >
-                    .map()
-                </Typography>
-                <SyntaxHighlighter language="jsx" style={materialDark}>
-                    {map.simpleExample}
-                </SyntaxHighlighter>
+                {renderSelectedExamples()}
             </Box>
-            <Box>
-                <Typography variant='subtitle1' sx={{ pt: 1 }}>
-                    .map() in a component with TypeScript
-                </Typography>
-                <SyntaxHighlighter language="jsx" style={materialDark}>
-                    {map.simpleAndTypeScriptExample}
-                </SyntaxHighlighter>
-            </Box>
-            <Typography variant='subtitle1' sx={{ pt: 1 }}>
-                .map() over array of objects while updating properties on the object in a component with TypeScript
-            </Typography>
-            <SyntaxHighlighter language="jsx" style={materialDark}>
-                {map.objectAndTypeScriptExample}
-            </SyntaxHighlighter>
-            <Typography variant='subtitle1' sx={{ pt: 1 }}>
-                .filter() and .map()
-            </Typography>
-            <SyntaxHighlighter language='jsx' style={materialDark}>
-                {filter.basicFilterAndMapExample}
-            </SyntaxHighlighter>
-            <Typography variant='subtitle1' sx={{ pt: 1 }}>
-                .filter() and .map() with TypeScript
-            </Typography>
-            <SyntaxHighlighter language='jsx' style={materialDark}>
-                {filter.typescriptFilterAndMapExample}
-            </SyntaxHighlighter>
-            <Typography variant='subtitle1' sx={{ pt: 1 }}>
-                .reduce()
-            </Typography>
-            <SyntaxHighlighter language='jsx' style={materialDark}>
-                {reduceExample}
-            </SyntaxHighlighter>
+
         </Box >
     )
 }
